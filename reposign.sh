c0fb60da68361907748a2340982622d0b4bd2559
Rich O'Hare <ohare2@llnl.gov>
2015-08-20 07:49:04 -0700

    added usage comment to reposign.sh

--- reposign.sh
+++ reposign.sh
@@ -3,6 +3,8 @@
 # $Date: $
 #
 # reposign.sh
+# This script needs to be run on the machines where the RPMs are built. RHEL5 signitures
+# do not work on RHEL6 and vise versa.
 
 # Sign rpm files
 cd ~/rpmbuild/RPMS/noarch/
