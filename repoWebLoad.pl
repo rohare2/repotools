59e1e1308a604dbe39b9d022c5c32ce38b34d79d
Rich O'Hare <ohare2@llnl.gov>
2015-09-03 12:34:33 -0700

    removed net loop

--- repoWebLoad.pl
+++ repoWebLoad.pl
@@ -8,7 +8,6 @@ use strict;
 use File::Copy;
 
 my $debug = 1;
-my @net = ('gs','hal','jwics');
 my $BASE_DIR = "/var/www/html/software";
 
 # RPMS source directory
@@ -25,47 +24,53 @@ if (not defined $dir) {
 
 # Push rpms to web server
 opendir(DIR, "$dir") or die "Can't open $dir";
-foreach my $net (@net) {
-	while (my $file = readdir(DIR)) {
-		 my ($distro,$release,$arch,$dest);
-		 if ($file =~ /^.*\.${net}\./) {
-			my $repoPath = ${BASE_DIR} . "/" . $net;
-			if ($file =~ /\.redhat5_/) {
-				$distro = 'redhat';
-				$release = '5';
-			}
-			if ($file =~ /\.redhat6_/) {
-				$distro = 'redhat';
-				$release = '6';
-			}
-			if ($file =~ /\.redhat7_/) {
-				$distro = 'redhat';
-				$release = '7';
-			}
-			if ($file =~ /\.centos5_/) {
-				$distro = 'centos';
-				$release = '5';
-			}
-			if ($file =~ /\.centos6_/) {
-				$distro = 'centos';
-				$release = '6';
-			}
-			if ($file =~ /\.centos7_/) {
-				$distro = 'centos';
-				$release = '7';
-			}
-			defined $distro or next;
+while (my $file = readdir(DIR)) {
+	my ($net,$distro,$release,$arch,$dest);
 
-			$file =~ /_x86_64\./ && ($arch = 'x86_64');
-			$file =~ /_i386\./ && ($arch = 'i386');
-
-			$dest = $BASE_DIR . "/" . $net . "/" . $distro . "/" . $release . "/" . $arch;
-
-			$debug && print "install -D -m 644 -o apache -g apache $dir/$file $dest/$file\n";
-			`install -D -m 644 -o apache -g apache $dir/$file $dest/$file`;
-		} else {
-			next;
-		}
+	if ($file =~ /\.gs\./) {
+	 	$net = 'gs';
 	}
+	if ($file =~ /\.hal\./) {
+	 	$net = 'hal';
+	}
+	if ($file =~ /\.jwics\./) {
+		$net = 'jwics';
+	}
+	defined $net or next;
+
+	if ($file =~ /\.redhat5_/) {
+		$distro = 'redhat';
+		$release = '5';
+	}
+	if ($file =~ /\.redhat6_/) {
+		$distro = 'redhat';
+		$release = '6';
+	}
+	if ($file =~ /\.redhat7_/) {
+		$distro = 'redhat';
+		$release = '7';
+	}
+	if ($file =~ /\.centos5_/) {
+		$distro = 'centos';
+		$release = '5';
+	}
+	if ($file =~ /\.centos6_/) {
+		$distro = 'centos';
+		$release = '6';
+	}
+	if ($file =~ /\.centos7_/) {
+		$distro = 'centos';
+		$release = '7';
+	}
+	defined $distro or next;
+
+	$file =~ /_x86_64\./ && ($arch = 'x86_64');
+	$file =~ /_i386\./ && ($arch = 'i386');
+	defined $arch or next;
+
+	$dest = $BASE_DIR . "/" . $net . "/" . $distro . "/" . $release . "/" . $arch;
+
+	$debug && print "install -D -m 644 $dir/$file $dest/$file\n";
+	`install -D -m 644 $dir/$file $dest/$file`;
 }
 close DIR;
